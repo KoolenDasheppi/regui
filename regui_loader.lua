31a50c46dec23618351c04f621c86a0a288fd6f9
HexaRG <67880600+HexaRG@users.noreply.github.com>
2020-07-17 21:23:02 -0400

    fixed detection

--- regui_loader.lua
+++ regui_loader.lua
@@ -1,51 +1,54 @@
+--TODO: create custom debug output because printing text can be detected
+local function output(text)
+	
+end
+
 local http = game:GetService("HttpService")
 local url = "https://raw.githubusercontent.com/HexaRG/regui/master/"
 function download(file)
 	return game:HttpGet(url .. file, true)
 end
 function downloadscripts()
-	print("downloading regui scrips from github")
+	output("downloading regui scrips from github")
 	local scripts_to_download = http:JSONDecode(download("scripts.json"))
 	for i,script in pairs(scripts_to_download) do
 		writefile("regui/scripts/" .. script,download(script))
 	end
 end
-print("checking for regui folder")
+output("checking for regui folder")
 local container_exists = isfolder("regui")
 if not container_exists then
-	print("regui doesnt exist creating folder")
+	output("regui doesnt exist creating folder")
 	makefolder("regui")
 	makefolder("regui/scripts")
 	writefile("regui/version.txt",download("version.txt"))
 	downloadscripts()
 else
-	print("checking for updates")
+	output("checking for updates")
 	local loc_version = readfile("regui/version.txt")
 	local lat_version = download("version.txt")
 	if loc_version ~= lat_version then
-		print("updating")
+		output("updating")
 		local scripts = listfiles("regui\\scripts")
-		print("clearing scripts")
+		output("clearing scripts")
 		for i,path in  pairs(scripts) do
 			delfile(path)
 		end
 		downloadscripts()
 		writefile("regui/version.txt",lat_version)
 	else
-		print("running the latest version")
+		output("running the latest version")
 	end
 end
-print("retrieving files located in regui\\scripts")
+output("retrieving files located in regui\\scripts")
 local scripts = listfiles("regui\\scripts")
-print("executing scripts found in regui\\scripts")
+output("executing scripts found in regui\\scripts")
 for i,path in  pairs(scripts) do
-	print("reading " .. path)
+	output("reading " .. path)
 	local script = readfile(path)
-	print("executing " .. path)
+	output("executing " .. path)
 	coroutine.resume(coroutine.create(function(source)
 		loadstring(source)()
 	end),script)
-	print("executed " .. path)
+	output("executed " .. path)
 end
---local playerlist = readfile("regui/playerlist.lua")
---local playerlist = readfile("regui/playerlist.lua")
