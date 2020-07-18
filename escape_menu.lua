f892fc188b444ae0be5b565f83462f009ec59248
HexaRG <67880600+HexaRG@users.noreply.github.com>
2020-07-18 04:00:12 -0400

    now displays the text "unfinished" in the RGsettings

--- escape_menu.lua
+++ escape_menu.lua
@@ -1,6 +1,19 @@
+wait(1)
 local robloxGui = game:GetService("CoreGui"):WaitForChild("RobloxGui")
-local hubBar = robloxGui.SettingsShield:WaitForChild("SettingsShield"):WaitForChild("MenuContainer"):WaitForChild("HubBar")
+local hubBar = robloxGui:WaitForChild("SettingsShield"):WaitForChild("SettingsShield"):WaitForChild("MenuContainer"):WaitForChild("HubBar")
 local pageView = robloxGui.SettingsShield.SettingsShield.MenuContainer:WaitForChild("PageViewClipper"):WaitForChild("PageView")
+local pageViewInnerFrame = pageView:WaitForChild("PageViewInnerFrame")
+local reguiInnerFrame = pageViewInnerFrame:Clone()
+syn.protect_gui(reguiInnerFrame)
+reguiInnerFrame.Parent = pageViewInnerFrame.Parent
+reguiInnerFrame:ClearAllChildren()
+local txtTest = Instance.new("TextLabel",reguiInnerFrame)
+txtTest.Text = "unfinished"
+txtTest.BackgroundTransparency = 1
+txtTest.TextColor3 = Color3.fromRGB(255,255,255)
+txtTest.TextScaled = true
+txtTest.Size = UDim2.new(1,0,1,0)
+robloxGui.SettingsShield.SettingsShield.BackgroundColor3 = Color3.fromRGB(70, 75, 86)
 local reguiSettingsHub = hubBar:WaitForChild("GameSettingsTab"):Clone()
 reguiSettingsHub.Parent = hubBar
 reguiSettingsHub.Name = "RGSettingsTab"
@@ -8,6 +21,7 @@ reguiSettingsHub.Icon.Title.Text = "REGUI Settings"
 reguiSettingsHub.Icon.Title.TextSize = 12
 reguiSettingsHub.Icon.Title.TextWrapped = 12
 reguiSettingsHub.Icon.Image = "rbxassetid://5366905099"
+syn.protect_gui(reguiSettingsHub)
 for _,HUB in pairs(hubBar:GetChildren()) do
     if not HUB:IsA("UIListLayout") then
         HUB.Size = UDim2.new(0.165,0,1,0)
@@ -20,7 +34,13 @@ local function setActive(hubItem)
     hubItem.Icon.Title.TextTransparency = 0
     hubItem.TabSelection.Visible = true
     if hubItem == reguiSettingsHub then
-        pageView.Visible = false
+        pageViewInnerFrame.Visible = false
+        reguiInnerFrame.Visible = true
+        local innerPlayer = pageViewInnerFrame:FindFirstChild("Players")
+        if innerPlayer then
+            game:GetService("RunService").RenderStepped:Wait()
+            innerPlayer:FindFirstChild("ImageButton").Visible = false
+        end
     end
 end
 
@@ -29,7 +49,13 @@ local function setUnactive(hubItem)
     hubItem.Icon.Title.TextTransparency = .5
     hubItem.TabSelection.Visible = false
     if hubItem == reguiSettingsHub then
-        pageView.Visible = true
+        pageViewInnerFrame.Visible = true
+        reguiInnerFrame.Visible = false
+        local innerPlayer = pageViewInnerFrame:FindFirstChild("Players")
+        if innerPlayer then
+            game:GetService("RunService").RenderStepped:Wait()
+            innerPlayer:FindFirstChild("ImageButton").Visible = true
+        end
     end
 end
 
