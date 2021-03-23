b13183a0268109c8d6adc623f92f7e9c0e255cf5
HexaRG <67880600+HexaRG@users.noreply.github.com>
2021-03-23 15:59:06 -0400

    fix i think

--- REGUI-Loader.lua
+++ REGUI-Loader.lua
@@ -1,44 +1,44 @@
 --//ReGui loader
 local ReGui = {
-	GithubUrl = "https://raw.githubusercontent.com/HexaRG/regui/rewrite/";
-	Directory = "ReGUI_V2"
-	Helper = {}
+	GithubUrl = "https://raw.githubusercontent.com/HexaRG/regui/rewrite";
+	Directory = "ReGUI_V2";
+	Helper = {};
 	Update = function(self,RemoveOldDir)
-		self.Helper:MakeFolder
 		--//Check for the ReGUI_V2
-		local ReGuiExists = isfolder(ReGui.Directory)
+		local ReGuiExists = self.Helper.Io:IsFolder(self.Directory)
 		if not ReGuiExists then
 			--//Create the ReGUI_V2 directory
-			self.Helper.Io:MakeFolder(ReGui.Directory)
+			self.Helper.Io:MakeFolder(self.Directory)
 			--//Create the Data directory
-			self.Helper.Io:MakeFolder(table.concat({ReGui.Directory,"Data"},"/"))
+			self.Helper.Io:MakeFolder(table.concat({self.Directory,"Data"},"/"))
 			--//Write LocalVersion.txt
 			self.Helper.Io:Write(
-				table.concat({ReGui.Directory,"LocalVersion.txt"},"/"),
+				table.concat({self.Directory,"LocalVersion.txt"},"/"),
 				self.Helper.Http:Get(
-					table.concat({ReGui.GithubUrl,"Data","LatestVersion.txt"},"/")
+					table.concat({self.GithubUrl,"Data","LatestVersion.txt"},"/")
 				)
 			)
 		end
 		local LocalVersion = self.Helper.Io:Read(
-			table.concat({ReGui.Directory,"LocalVersion.txt"},"/")
+			table.concat({self.Directory,"LocalVersion.txt"},"/")
 		)
 		local LatestVersion = self.Helper.Http:Get(
-			table.concat({ReGui.GithubUrl,"Data","LatestVersion.txt"},"/")
+			table.concat({self.GithubUrl,"Data","LatestVersion.txt"},"/")
 		)
+		print(LocalVersion,LatestVersion)
 		if LocalVersion ~= LatestVersion then
 			--//Update LocalVersion.txt
 			self.Helper.Io:Write(
-				table.concat({ReGui.Directory,"LocalVersion.txt"},"/"),
+				table.concat({self.Directory,"LocalVersion.txt"},"/"),
 				LatestVersion
 			)
 			--//Get install instructions
 			local InstallInstructions = self.Helper.Http:Get(
-				table.concat({ReGui.GithubUrl,"Data","Install.json"},"/"),
+				table.concat({self.GithubUrl,"Data","Install.json"},"/"),
 				true
 			)
 			--//Remove old Data folder
-			self.Helper.Io:DeleteFolder(table.concat({ReGui.GithubUrl,"Data"})
+			self.Helper.Io:DeleteFolder(table.concat({self.GithubUrl,"Data"}))
 			--//Download files
 			for InstructionId,IOInstruction in pairs(InstallInstructions) do
 				--[[
@@ -48,21 +48,21 @@ local ReGui = {
 						2: Folder
 					2 : Path (String)
 				]]
-				if IOInstruction[1] = 1 then
+				if IOInstruction[1] == 1 then
 					self.Helper.Io:Write(
-						table.concat({ReGui.GithubUrl,IOInstruction[2]},"/"),
+						table.concat({self.GithubUrl,IOInstruction[2]},"/"),
 						self.Helper.Http:Get(
-							table.concat({ReGui.GithubUrl,"Data",IOInstruction[2]},"/")
+							table.concat({self.GithubUrl,"Data",IOInstruction[2]},"/")
 						)
 					)
-				elseif IOInstruction[1] = 2 then
+				elseif IOInstruction[1] == 2 then
 					self.Helper.Io:MakeFolder(
-						table.concat({ReGui.GithubUrl,"Data",IOInstruction[2]},"/")
+						table.concat({self.GithubUrl,"Data",IOInstruction[2]},"/")
 					)
 				end
 			end
 		end
-	end
+	end;
 }
 --//Helpers
 
@@ -70,11 +70,13 @@ local ReGui = {
 
 local HttpService = game:GetService("HttpService")
 
+ReGui.Helper.Http = {}
+
 function ReGui.Helper.Http:Get(Url,AutoDecode)
 	if not AutoDecode then
 		return game:HttpGet(Url, true)
 	else
-		return ReGui.Helper.Http:JSONDecode(game:HttpGet(Url, true))
+		return self.Helper.Http:JSONDecode(game:HttpGet(Url, true))
 	end
 end
 
@@ -88,18 +90,28 @@ end
 
 --//Io
 
+ReGui.Helper.Io = {}
+
 function ReGui.Helper.Io:MakeFolder(NewFolder)
 	makefolder(NewFolder)
 end
+
 function ReGui.Helper.Io:DeleteFolder(NewFolder)
 	delfolder(NewFolder)
 end
+
+function ReGui.Helper.Io:IsFolder(NewFolder)
+	return isfolder(NewFolder)
+end
+
 function ReGui.Helper.Io:Write(File,Contents)
 	writefile(File,Contents)
 end
+
 function ReGui.Helper.Io:Read(File)
-	readfile(File)
+	return readfile(File)
 end
+
 
 ReGui:Update()
 
