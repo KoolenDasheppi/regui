e4cb5374262e1d2fddc7e94d399a7b47cfa580cc
HexaRG <67880600+HexaRG@users.noreply.github.com>
2021-03-24 16:50:59 -0400

    wip

--- Data/Main.lua
+++ Data/Main.lua
@@ -1,6 +1,6 @@
 return function(LocalPath)
     _ReGui:Log("Data/Main.lua started")
     _ReGui:Log("Load: PluginHandler")
-    _ReGui.PluginHandler = _ReGui.Helper:Require(_ReGui.Helper.Path:Join(_ReGui.Directory,"Data","Modules"),"PluginHandler.lua")
+    _ReGui.PluginHandler = _ReGui.Helper:Require(_ReGui.Helper.Path:Join(LocalPath,"Modules"),"PluginHandler.lua")
     _ReGui:Log("Loaded: PluginHandler")
 end
